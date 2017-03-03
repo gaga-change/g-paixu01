da20629725cf52f9efcd939fba2f3b18f629d80a
严俊东 <1172482914@qq.com>
2017-03-03 20:13:30 +0800

    引入数据请求

--- config/index.js
+++ config/index.js
@@ -27,7 +27,9 @@ module.exports = {
     autoOpenBrowser: true,
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
+    proxyTable: {
+      '/api': {target: 'http://mn.5173.cn', changeOrigin: true}
+    },
     // CSS Sourcemaps off by default because relative paths are "buggy"
     // with this option, according to the CSS-Loader README
     // (https://github.com/webpack/css-loader#sourcemaps)
