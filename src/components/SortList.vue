27dafb7b4596d969c8dae79dd36f45a05b12ca12
严俊东 <1172482914@qq.com>
2017-03-03 10:19:35 +0800

    菜单功能部分处理完成

--- src/components/SortList.vue
+++ src/components/SortList.vue
@@ -8,7 +8,10 @@
         <span v-text="query.areaname"></span><span v-if="query.servername">/</span>
         <span v-text="query.servername"></span>
       </vue-head>
-      <vue-menu></vue-menu>
+      <vue-menu :menu-son="menuSon" @clickMenu="clickMenu">
+        <span slot="menu-left"><cite>排序</cite><img src="/images/down3.png"></span>
+        <span slot="menu-right"><cite>筛选</cite><img src="/images/filter.png"></span>
+      </vue-menu>
     </div>
     <!-- 列表为空 -->
     <div class="list-main2 ">
@@ -37,7 +40,7 @@
       return {
         /**
          * query对象
-         * @returns {
+         *{
        *  gid<string> 游戏ID
        *  gname<string> 游戏名
        *  areaid<string> 游戏区 ID
@@ -54,25 +57,66 @@
        *  gameImageUrl<string>
        * }
          */
-        query: null
+        query: null,
+        /**
+         * 子菜单
+         * {
+         *    排序方式
+         *    sortWay <Array [Object]>
+         * }
+         */
+        menuSon: null
       }
     },
     created: function () {
-
-      this.query = this.$route.query;
-      console.log(this.query);
-    },
-    computed: {
-
-      query: function () {
-//        console.log("query: ", this.$route.query);
-//        return
-      }
+      // 保证在set的方法里设置的值必须正确，确保数据源头结构改变时不会动全身
+      this.setQuery();
+      this.setMenu();
     },
     components: {
       'vue-head': Head,
       'vue-menu': Menu,
       'vue-list': List
+    },
+    methods: {
+      /**
+       * 从链接中提取参数
+       * */
+      setQuery: function () {
+        this.query = this.$route.query;
+      },
+      /**
+       * 手动配置子菜单
+       */
+      setMenu: function () {
+        this.menuSon = {
+          sortWay: [
+            {
+              name: "默认排序（发布时间排序）",
+              sortMap: {
+                create_time: '-'
+              }
+            }, {
+              name: "热门排序（浏览量高到低）",
+              sortMap: {
+                browse_count: '-'
+              }
+            }, {
+              name: "价格由高到低",
+              sortMap: {
+                price: '-'
+              }
+            }, {
+              name: "价格由低到高",
+              sortMap: {
+                price: '+'
+              }
+            }]
+        }
+      },
+      clickMenu: function (item) {
+        console.log(item);
+      }
     }
   }
 </script>
--- src/components/SortList_Menu.vue
+++ src/components/SortList_Menu.vue
@@ -3,21 +3,24 @@
   <div class="list-select ">
     <div class="select-main border-bottom">
       <div class="select-left">
-        <a href="javascript:;" class="divs" id="divselect2">
-          <span><cite>排序</cite><img src="/images/down3.png"></span>
+        <a href="javascript:;" class="divs" id="divselect2" @click="sortWayShow = !sortWayShow">
+          <slot name="menu-left"></slot>
         </a>
       </div>
       <div class="select-right">
         <div id="divselect3">
-          <span><cite>筛选</cite><img src="/images/filter.png"></span>
+          <slot name="menu-right"></slot>
         </div>
       </div>
     </div>
-    <div class="divc showcata2 border-top">
+    <div v-if="sortWayShow" class="divc showcata2 border-top" style="display: block"
+         @click="clickSortWay($event, 'sortWay')">
       <ul>
-        <li><a href="javascript:;" selectid="1">热门排序</a></li>
-        <li><a href="javascript:;" selectid="2">价格从低到高</a></li>
-        <li><a href="javascript:;" selectid="3">价格从高到底</a></li>
+        <li v-for="(item,index) in menuSon.sortWay"><a href="javascript:;" :data-index="index" v-text="item.name"></a>
+        </li>
+        <!--<li><a href="javascript:;" selectid="1">热门排序</a></li>-->
+        <!--<li><a href="javascript:;" selectid="2">价格从低到高</a></li>-->
+        <!--<li><a href="javascript:;" selectid="3">价格从高到底</a></li>-->
       </ul>
       <input name="" type="hidden" value="" id="inputselect2"/>
     </div>
@@ -25,7 +28,25 @@
 </template>
 
 <script>
-  export default  {}
+  export default  {
+    props: ['menuSon'],
+    data: function () {
+      return {
+        sortWay: this.menuSon.sortWay,
+        sortWayShow: false
+      }
+    },
+    methods: {
+      clickSortWay: function (e) {
+        this.sortWayShow = false;
+        this.clickMenu(this.menuSon.sortWay[e.target.attributes['data-index'].value]);
+      },
+      clickMenu: function (item) {
+        this.$emit('clickMenu', item);
+      }
+    }
+
+  }
 </script>
 
 <style>
