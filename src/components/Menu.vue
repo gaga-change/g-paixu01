a1fbe7b169f554c48522ba0a67c968ccfb91e1fe
严俊东 <1172482914@qq.com>
2017-03-06 13:33:12 +0800

    辅助菜单添加样式，测试页面路由变化

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,14 +1,24 @@
 <template>
   <div>
+    <h1>辅助菜单</h1>
     <ul>
       <li>
-        <router-link to="/?gid=YX16053122463418100536&gname=英雄联盟&areaid=YXQ16053123494177201414&areaname=电信&operatorId=&operatorName=&raceid=&racename=undefined&serverid=YXF16053123502273501422&servername=艾欧尼亚&goodsType=2&typename=帐号&gameImageUrl=">艾欧尼亚</router-link>
+        <router-link
+          to="/sort-list/?gid=YX16053122463418100536&gname=英雄联盟&areaid=YXQ16053123494177201414&areaname=电信&operatorId=&operatorName=&raceid=&racename=undefined&serverid=YXF16053123502273501422&servername=艾欧尼亚&goodsType=2&typename=帐号&gameImageUrl=">
+          艾欧尼亚
+        </router-link>
       </li>
       <li>
-        <router-link to="/?gid=YX16053122463418100536&gname=英雄联盟&areaid=YXQ16053123494177201414&areaname=电信&operatorId=&operatorName=&raceid=&racename=undefined&serverid=YXF16053123522672101468&servername=祖安&goodsType=2&typename=帐号&gameImageUrl=">祖安</router-link>
+        <router-link
+          to="/sort-list/?gid=YX16053122463418100536&gname=英雄联盟&areaid=YXQ16053123494177201414&areaname=电信&operatorId=&operatorName=&raceid=&racename=undefined&serverid=YXF16053123522672101468&servername=祖安&goodsType=2&typename=帐号&gameImageUrl=">
+          祖安
+        </router-link>
       </li>
       <li>
-        <router-link to="/?gid=YX16053122463418100536&gname=英雄联盟&areaid=YXQ16053123494177201414&areaname=电信&operatorId=&operatorName=&raceid=&racename=undefined&serverid=YXF16053123521594001462&servername=影流&goodsType=2&typename=帐号&gameImageUrl=">流影</router-link>
+        <router-link
+          to="/sort-list/?gid=YX16053122463418100536&gname=英雄联盟&areaid=YXQ16053123494177201414&areaname=电信&operatorId=&operatorName=&raceid=&racename=undefined&serverid=YXF16053123521594001462&servername=影流&goodsType=2&typename=帐号&gameImageUrl=">
+          流影
+        </router-link>
       </li>
     </ul>
   </div>
@@ -18,6 +28,25 @@
   export default{}
 </script>
 
-<style>
+<style scoped>
+  h1 {
+    text-align: center;
+    margin-top: 2em;
+  }
+  ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+  }
+
+  ul > li {
+    border-bottom: 1px solid gainsboro;
+    padding: 20px 10px;
+  }
+
+  ul > li:nth-child(even) {
+    background-color: #bbefa0;
+  }
+
 
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -10,7 +10,7 @@ export default new Router({
   mode: "history",
   routes: [
     {
-      path: '/',
+      path: '/sort-list',
       name: 'SortList',
       component: SortList
     }, {
