b97f3bfa922ef7bdbfc51e97b801c665c2c11597
严俊东 <1172482914@qq.com>
2017-03-06 14:54:22 +0800

    返回处理了一下

--- src/components/SortList_Head.vue
+++ src/components/SortList_Head.vue
@@ -1,9 +1,10 @@
 <template>
   <div class="top-header border-bottom ">
     <div class="top-back">
-      <a href="../index.html">返回</a>
+      <!--<a :href="$route.go(-1)">返回</a>-->
+      <router-link :to="{name:'menu'}">返回</router-link>
     </div>
-    <div class="top-bt f32" style="width: 100%" >
+    <div class="top-bt f32" style="width: 100%">
       <slot>地下城与勇士/电信二区/豪杰永生</slot>
     </div>
   </div>
